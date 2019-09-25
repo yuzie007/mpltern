e967a771addfe7fec645375b342aad170e89fb0a
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2019-09-25 18:45:02 +0200

    Add new transforms for axis labels and data

--- taxes/transforms.py
+++ taxes/transforms.py
@@ -21,7 +21,7 @@ class TernaryTransform(Transform):
 
     def __init__(self, corners, index, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.corners = corners
+        self.corners = np.asarray(corners)
         self.index = index
 
     def transform_non_affine(self, points):
@@ -47,7 +47,7 @@ class InvertedTernaryTransform(Transform):
 
     def __init__(self, corners, index, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.corners = corners
+        self.corners = np.asarray(corners)
         self.index = index
 
     def transform_non_affine(self, points):
@@ -67,3 +67,104 @@ class InvertedTernaryTransform(Transform):
 
     def inverted(self):
         return TernaryTransform(self.corners, self.index)
+
+
+class VerticalTernaryTransform(Transform):
+    """Transform convenient for axis-labels in TernaryAxis.
+
+    Input
+    -----
+    0 <= x <= 1
+    x == 0 corresponds to ternary min.
+    x == 1 corresponds to ternary max.
+
+    y : float
+        Vertical shift from the ternary axis in the `display`
+        coordinate system.
+
+    Output
+    ------
+    """
+    input_dims = 2
+    output_dims = 2
+    has_inverse = True
+
+    def __init__(self, trans, corners, index, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.trans = trans
+        self.corners = np.asarray(corners)
+        self.index = index
+
+    def transform_non_affine(self, points):
+        corners = self.trans.transform(self.corners)
+        c0 = corners[(self.index + 0) % 3]
+        c1 = corners[(self.index + 1) % 3]
+        v0 = c1 - c0
+        v1 = [-v0[1], v0[0]]  # Vertical
+        v1 /= np.linalg.norm(v1)
+        v = np.column_stack((v0, v1))
+        return c0 + np.dot(v, points.T).T
+
+    def inverted(self):
+        return InvertedVerticalTernaryTransform(self.trans, self.corners, self.index)
+
+
+class InvertedVerticalTernaryTransform(Transform):
+    input_dims = 2
+    output_dims = 2
+    has_inverse = True
+
+    def __init__(self, trans, corners, index, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.trans = trans
+        self.corners = np.asarray(corners)
+        self.index = index
+
+    def transform_non_affine(self, points):
+        corners = self.trans.transform(self.corners)
+        c0 = corners[(self.index + 0) % 3]
+        c1 = corners[(self.index + 1) % 3]
+        v0 = c1 - c0
+        v1 = [-v0[1], v0[0]]
+        v1 /= np.linalg.norm(v1)
+        v = np.column_stack((v0, v1))
+        d = points - c0
+        tmp = np.dot(np.linalg.inv(v), d.T).T
+        return tmp
+
+    def inverted(self):
+        return VerticalTernaryTransform(self.corners, self.index)
+
+
+class TernaryDataTransform(Transform):
+    input_dims = 3
+    output_dims = 2
+    has_inverse = True
+
+    def __init__(self, scale, corners, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.scale = scale
+        self.corners = np.asarray(corners)
+
+    def transform_non_affine(self, points):
+        c = np.roll(self.corners, shift=-1, axis=0)
+        return np.dot(points, c) / self.scale
+
+    def inverted(self):
+        return InvertedTernaryDataTransform(self.corners)
+
+
+class InvertedTernaryDataTransform(Transform):
+    input_dims = 2
+    output_dims = 3
+    has_inverse = True
+
+    def __init__(self, corners, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.corners = np.asarray(corners)
+
+    def transform_non_affine(self, points):
+        raise NotImplementedError
+
+    def inverted(self):
+        return TernaryDataTransform(self.corners)
