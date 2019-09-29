c1367c13586d0a566d06bbeb334bef5332158770
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2019-09-29 18:19:21 +0200

    Fix to remove the test for the old TernaryDataTransform

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,28 +1,12 @@
 import numpy as np
 import pytest
-from taxes.transforms import TernaryDataTransform, BarycentricTransform
+from taxes.transforms import BarycentricTransform
 
 
 corners_list = [
     ((0.5, 0.0), (1.0, 0.5), (0.0, 1.0)),
     ((0.0, 0.0), (1.0, 0.0), (0.5, 1.0)),
 ]
-
-@pytest.mark.parametrize('scale', [1.0, 2.0, -1.0, -2.0])
-@pytest.mark.parametrize('corners', corners_list)
-def test_ternary_data_transform(scale, corners):
-    np.random.seed(19860107)
-    points = np.random.rand(300).reshape(-1, 3)
-    points *= (scale / np.sum(points, axis=1)[:, None])
-    # print(points)
-
-    trans = TernaryDataTransform(scale, corners)
-
-    points_transformed = trans.transform(points)
-    # print(points_transformed)
-
-    np.testing.assert_almost_equal(
-        points, trans.inverted().transform(points_transformed))
 
 
 @pytest.mark.parametrize('scale', [1.0, 2.0, -1.0, -2.0])
