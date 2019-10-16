5907d850b1e2b2f9ee134e9d7c597a8629d317e1
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2019-10-16 08:32:04 +0200

    Add the examples for spans

---
+++ examples/spans.py
@@ -0,0 +1,36 @@
+"""
+=====
+Spans
+=====
+
+A line or a band horizontal to an ternary axis can be plotted by the folowing
+commands similarly to ``axhline``, ``axvline``, ``axhspan``, ``axvspan`` in
+Matplotlib.
+
+- ``axtline``
+- ``axlline``
+- ``axrline``
+- ``axtspan``
+- ``axlspan``
+- ``axrspan``
+"""
+import matplotlib.pyplot as plt
+import mpltern  # noqa: F401
+
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='ternary')
+
+ax.set_tlabel('Top')
+ax.set_llabel('Left')
+ax.set_rlabel('Right')
+
+ax.axtline(0.2, c='C0')
+ax.axlline(0.3, c='C1')
+ax.axrline(0.4, c='C2')
+
+ax.axtspan(0.3, 0.5, fc='C0', alpha=0.2)
+ax.axlspan(0.4, 0.6, fc='C1', alpha=0.2)
+ax.axrspan(0.5, 0.7, fc='C2', alpha=0.2)
+
+plt.show()
