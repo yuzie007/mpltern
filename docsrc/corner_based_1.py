c2f00d6faeea77ebd07012567a779c1b7d1a8cbc
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2019-10-19 12:00:42 +0200

    Update the figures to explain the conventions in Mpltern

--- docsrc/corner_based_1.py
+++ docsrc/corner_based_1.py
@@ -1,63 +1,56 @@
-import numpy as np
-
 import matplotlib.pyplot as plt
-import mpltern
+import mpltern  # noqa: F401
 
 
-fig = plt.figure(figsize=(10.8, 3.2))
+fig = plt.figure(figsize=(10.8, 4.0))
 fig.subplots_adjust(
-    left=0.075, right=0.925, bottom=0.125, top=0.825, wspace=0.65)
+    left=0.075, right=0.925, bottom=0.15, top=0.9, wspace=0.5)
 
 # Top axis
 
 ax = fig.add_subplot(131, projection='ternary')
+ax.scatter(0.6, 0.2, 0.2, c='C3', zorder=10)
 
-ax.set_rlabel('Top')
-ax.raxis.label.set_color('C0')
+ax.set_tlabel('Top')
+ax.taxis.label.set_color('C0')
 
 ax.laxis.set_ticklabels([])
-ax.baxis.set_ticklabels([])
+ax.raxis.set_ticklabels([])
 
-ax.grid(True)
-ax.grid(axis='r', lw=2.4)
+ax.grid()
 
-ax.raxis.set_tick_params(colors='C0', grid_color='C0')
-
-ax.raxis.set_label_position('corner')
+ax.taxis.set_tick_params(colors='C0', grid_color='C0', grid_linewidth=2.4)
 
 # Left axis
 
 ax = fig.add_subplot(132, projection='ternary')
+ax.scatter(0.6, 0.2, 0.2, c='C3', zorder=10)
 
 ax.set_llabel('Left')
 ax.laxis.label.set_color('C1')
 
-ax.baxis.set_ticklabels([])
 ax.raxis.set_ticklabels([])
+ax.taxis.set_ticklabels([])
 
-ax.grid(True)
-ax.grid(axis='l', lw=2.4)
+ax.grid()
 
-ax.laxis.set_tick_params(colors='C1', grid_color='C1')
-
-ax.laxis.set_label_position('corner')
+ax.laxis.set_tick_params(colors='C1', grid_color='C1', grid_linewidth=2.4)
 
 # Right axis
 
 ax = fig.add_subplot(133, projection='ternary')
+ax.scatter(0.6, 0.2, 0.2, c='C3', zorder=10)
 
-ax.set_blabel('Right')
-ax.baxis.label.set_color('C2')
+ax.set_rlabel('Right')
+ax.raxis.label.set_color('C2')
 
-ax.raxis.set_ticklabels([])
+ax.taxis.set_ticklabels([])
 ax.laxis.set_ticklabels([])
 
-ax.grid(True)
-ax.grid(axis='b', lw=2.4)
+ax.grid()
 
-ax.baxis.set_tick_params(colors='C2', grid_color='C2')
-
-ax.baxis.set_label_position('corner')
+ax.raxis.set_tick_params(colors='C2', grid_color='C2', grid_linewidth=2.4)
 
 fig.suptitle('Corner-based perspective')
+
 fig.savefig('corner_based_1.svg')
--- docsrc/side_based_cw.py
+++ docsrc/side_based_cw.py
@@ -1,63 +1,65 @@
-import numpy as np
-
 import matplotlib.pyplot as plt
-import mpltern
+import mpltern  # noqa: F401
 
 
-fig = plt.figure(figsize=(10.8, 3.2))
+fig = plt.figure(figsize=(10.8, 4.0))
 fig.subplots_adjust(
-    left=0.075, right=0.925, bottom=0.125, top=0.825, wspace=0.65)
+    left=0.075, right=0.925, bottom=0.15, top=0.9, wspace=0.5)
 
 # Top axis
 
-ax = fig.add_subplot(131, projection='ternary')
+ax = fig.add_subplot(132, projection='ternary')
+ax.scatter(0.6, 0.2, 0.2, c='C3', zorder=10)
 
-ax.set_blabel('Right')
-ax.baxis.label.set_color('C0')
+ax.set_tlabel('Left')
+ax.taxis.label.set_color('C1')
+ax.taxis.set_label_position('tick2')
 
-ax.raxis.set_ticklabels([])
 ax.laxis.set_ticklabels([])
+ax.raxis.set_ticklabels([])
 
-ax.grid(True)
-ax.grid(axis='b', lw=2.4)
+ax.grid()
 
-ax.baxis.set_tick_params(colors='C0', grid_color='C0')
+ax.taxis.set_tick_params(colors='C1', grid_color='C1', grid_linewidth=2.4)
 
 ax.opposite_ticks(True)
 
 # Left axis
 
-ax = fig.add_subplot(132, projection='ternary')
+ax = fig.add_subplot(133, projection='ternary')
+ax.scatter(0.6, 0.2, 0.2, c='C3', zorder=10)
 
-ax.set_rlabel('Left')
-ax.raxis.label.set_color('C1')
+ax.set_llabel('Bottom')
+ax.laxis.label.set_color('C2')
+ax.laxis.set_label_position('tick2')
 
-ax.laxis.set_ticklabels([])
-ax.baxis.set_ticklabels([])
+ax.raxis.set_ticklabels([])
+ax.taxis.set_ticklabels([])
 
-ax.grid(True)
-ax.grid(axis='r', lw=2.4)
+ax.grid()
 
-ax.raxis.set_tick_params(colors='C1', grid_color='C1')
+ax.laxis.set_tick_params(colors='C2', grid_color='C2', grid_linewidth=2.4)
 
 ax.opposite_ticks(True)
 
 # Right axis
 
-ax = fig.add_subplot(133, projection='ternary')
+ax = fig.add_subplot(131, projection='ternary')
+ax.scatter(0.6, 0.2, 0.2, c='C3', zorder=10)
 
-ax.set_llabel('Bottom')
-ax.laxis.label.set_color('C2')
+ax.set_rlabel('Right')
+ax.raxis.label.set_color('C0')
+ax.raxis.set_label_position('tick2')
 
-ax.baxis.set_ticklabels([])
-ax.raxis.set_ticklabels([])
+ax.taxis.set_ticklabels([])
+ax.laxis.set_ticklabels([])
 
-ax.grid(True)
-ax.grid(axis='l', lw=2.4)
+ax.grid()
 
-ax.laxis.set_tick_params(colors='C2', grid_color='C2')
+ax.raxis.set_tick_params(colors='C0', grid_color='C0', grid_linewidth=2.4)
 
 ax.opposite_ticks(True)
 
 fig.suptitle('Side-based perspective with clockwise ticks')
+
 fig.savefig('side_based_cw.svg')
