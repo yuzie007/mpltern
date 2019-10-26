e0bcdc7c50085ad5df27b39883a29c822d53bce8
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2019-10-26 22:07:38 +0200

    Fix to update transforms of tick labels when locations are updated
    Previously the pads of tick labels from the axis and the tick markers
    were determined when the `Text` objects for tick labels are created and
    never updated afterwords. Actually when drawing the triangle geometry is
    updated and hence the pads should be also modified following the change
    of the triangle geometry. Now this is fixed, and the pads become
    consistent for all the axes.

--- mpltern/ternary/tick.py
+++ mpltern/ternary/tick.py
@@ -158,6 +158,11 @@ class TernaryTick(XTick):
         self._tilt_marker(self.tick1line, np.deg2rad(tick1_angle) - np.pi / 2)
         self._tilt_marker(self.tick2line, np.deg2rad(tick1_angle) + np.pi / 2)
 
+        # Since the triangle geometry is updated when drawing, transforms for
+        # texts must be also updated for consistent padding.
+        self.label1.set_transform(self._get_text1_transform()[0])
+        self.label2.set_transform(self._get_text2_transform()[0])
+
         # Tick labels
         mode, user_angle = self._labelrotation
 
