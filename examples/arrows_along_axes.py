992298e7569cc5abc0d59be58dc68aa84f1ff680
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2019-10-27 09:50:41 +0100

    Modify the example of arrows along axes to use `FancyArrowPatch`
    The previous way using `ax.arrow` was found not suitable for the arrows
    along axes because arrow heads etc. are not drawn in the display
    coordinates but scale with the given coordinates for data or `Axes`.
    Using `FancyArrowPatch`, arrow heads can be fixed in the display
    coordinates.

--- examples/arrows_along_axes.py
+++ examples/arrows_along_axes.py
@@ -3,43 +3,51 @@
 Arrows along axes
 =================
 
-Arrows along the axes can be put in barycentric coordinates using
-`ax.transTernaryAxes`.
-This should work as expected also in an interactive mode.
+Arrows along the axes can be put in barycentric coordinates using transforms
+for ``TernaryAxes`` in combination with ``FancyArrowPatch``.
 """
 import numpy as np
 
 import matplotlib.pyplot as plt
+from matplotlib.patches import ArrowStyle, FancyArrowPatch
 import mpltern  # noqa: F401
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='ternary')
+ax = plt.subplot(projection='ternary')
 
+arrowstyle = ArrowStyle('simple', head_length=10, head_width=5)
 kwargs_arrow = {
-    'transform': ax.transTernaryAxes,
-    'head_width': 0.02,
-    'length_includes_head': True,
+    'transform': ax.transAxes,  # Used with ``ax.transAxesProjection``
+    'arrowstyle': arrowstyle,
+    'linewidth': 1,
     'clip_on': False,  # To plot arrows outside triangle
-    'zorder': -10,  # Set very low value not to hide e.g. tick labels.
+    'zorder': -10,  # Very low value not to hide e.g. tick labels.
 }
 
-# Origins of the arrows in barycentric coordinates.
-to = np.array([ 0.00, -0.10,  1.10])
-lo = np.array([ 1.10,  0.00, -0.10])
-ro = np.array([-0.10,  1.10,  0.00])
+# Start of arrows in barycentric coordinates.
+ta = np.array([ 0.0, -0.1,  1.1])
+la = np.array([ 1.1,  0.0, -0.1])
+ra = np.array([-0.1,  1.1,  0.0])
 
-# Lengths of the arrows in barycentric coordinates.
-tl = np.array([ 1.0,  0.0, -1.0])
-ll = np.array([-1.0,  1.0,  0.0])
-rl = np.array([ 0.0, -1.0,  1.0])
+# End of arrows in barycentric coordinates.
+tb = np.array([ 1.0, -0.1,  0.1])
+lb = np.array([ 0.1,  1.0, -0.1])
+rb = np.array([-0.1,  0.1,  1.0])
 
-ax.arrow(*to, *tl, ec='C0', fc='C0', **kwargs_arrow)
-ax.arrow(*lo, *ll, ec='C1', fc='C1', **kwargs_arrow)
-ax.arrow(*ro, *rl, ec='C2', fc='C2', **kwargs_arrow)
+# This transforms the above barycentric coordinates to the original Axes
+# coordinates. In combination with ``ax.transAxes``, we can plot arrows fixed
+# to the Axes coordinates.
+f = ax.transAxesProjection.transform
 
-# # To put the axis-labels at the positions consistent with the arrows above,
-# # it may be better to put the axis-label-text directly as follows rather than
-# # using e.g.  ax.set_tlabel.
+tarrow = FancyArrowPatch(f(ta), f(tb), ec='C0', fc='C0', **kwargs_arrow)
+larrow = FancyArrowPatch(f(la), f(lb), ec='C1', fc='C1', **kwargs_arrow)
+rarrow = FancyArrowPatch(f(ra), f(rb), ec='C2', fc='C2', **kwargs_arrow)
+ax.add_patch(tarrow)
+ax.add_patch(larrow)
+ax.add_patch(rarrow)
+
+# To put the axis-labels at the positions consistent with the arrows above, it
+# may be better to put the axis-label-text directly as follows rather than
+# using e.g.  ax.set_tlabel.
 kwargs_label = {
     'transform': ax.transTernaryAxes,
     'backgroundcolor': 'w',
@@ -50,14 +58,12 @@ kwargs_label = {
 }
 
 # Put axis-labels on the midpoints of arrows.
-tpos = to + 0.5 * tl
-lpos = lo + 0.5 * ll
-rpos = ro + 0.5 * rl
+tpos = (ta + tb) * 0.5
+lpos = (la + lb) * 0.5
+rpos = (ra + rb) * 0.5
 
 ax.text(*tpos, 'Top'  , color='C0', rotation=-60, **kwargs_label)
 ax.text(*lpos, 'Left' , color='C1', rotation= 60, **kwargs_label)
 ax.text(*rpos, 'Right', color='C2', rotation=  0, **kwargs_label)
-
-ax.tick_params(labelrotation='tick')
 
 plt.show()
