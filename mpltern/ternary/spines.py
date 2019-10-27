9c31686981b385ad57c007831144808520572c2b
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2019-10-27 10:49:46 +0100

    Refactor spines not to use `Axes.corners` directly
    This may hopefully make the spines robust for the change of
    `Axes.corners`.

--- mpltern/ternary/spines.py
+++ mpltern/ternary/spines.py
@@ -7,27 +7,23 @@ class Spine(mspines.Spine):
         self.set_transform(self.axes.transAxes)
 
     def _adjust_location(self):
-        if True:
-            v1 = self._path.vertices
-            assert v1.shape == (2, 2), 'unexpected vertices shape'
-            if self.spine_type in ['top', 'bottom']:  # TODO
-                v1[0, 0] = self.axes.corners[1][0]
-                v1[0, 1] = self.axes.corners[1][1]
-                v1[1, 0] = self.axes.corners[2][0]
-                v1[1, 1] = self.axes.corners[2][1]
-            elif self.spine_type in ['right']:
-                v1[0, 0] = self.axes.corners[2][0]
-                v1[0, 1] = self.axes.corners[2][1]
-                v1[1, 0] = self.axes.corners[0][0]
-                v1[1, 1] = self.axes.corners[0][1]
-            elif self.spine_type in ['left']:
-                v1[0, 0] = self.axes.corners[0][0]
-                v1[0, 1] = self.axes.corners[0][1]
-                v1[1, 0] = self.axes.corners[1][0]
-                v1[1, 1] = self.axes.corners[1][1]
-            else:
-                raise ValueError('unable to set bounds for spine "%s"' %
-                                 self.spine_type)
+        corners = [[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]]
+        corners_axes = self.axes.transAxesProjection.transform(corners)
+
+        v1 = self._path.vertices
+        assert v1.shape == (2, 2), 'unexpected vertices shape'
+        if self.spine_type in ['top', 'bottom']:  # TODO
+            v1[0] = corners_axes[1]
+            v1[1] = corners_axes[2]
+        elif self.spine_type in ['right']:
+            v1[0] = corners_axes[2]
+            v1[1] = corners_axes[0]
+        elif self.spine_type in ['left']:
+            v1[0] = corners_axes[0]
+            v1[1] = corners_axes[1]
+        else:
+            raise ValueError('unable to set bounds for spine "%s"' %
+                             self.spine_type)
 
     def get_spine_transform(self):
         return self.axes.transAxes
