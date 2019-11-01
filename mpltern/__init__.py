fa8a8b74aeab1a8ee071f3e9c312bf185f670e48
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2019-11-01 11:20:09 +0100

    Modify to use versioneer

--- mpltern/__init__.py
+++ mpltern/__init__.py
@@ -1,4 +1,8 @@
 from matplotlib.projections import register_projection
 from mpltern.ternary import TernaryAxes
-__version__ = '0.2.0'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
 register_projection(TernaryAxes)
