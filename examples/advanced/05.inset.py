dddae91ae7a5d9685fad6f3d98c03fe1e579fa4d
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2019-11-01 12:50:13 +0100

    Modify to make markers translucent

--- examples/advanced/05.inset.py
+++ examples/advanced/05.inset.py
@@ -16,8 +16,8 @@ fig.subplots_adjust(left=-0.1)
 
 ax = fig.add_subplot(projection='ternary')
 
-ax.scatter(t0, l0, r0)
-ax.scatter(t1, l1, r1)
+ax.scatter(t0, l0, r0, alpha=0.5)
+ax.scatter(t1, l1, r1, alpha=0.5)
 
 # Plot the triangle region for the inset.
 ax.fill([0.4, 0.3, 0.3], [0.3, 0.4, 0.3], [0.3, 0.3, 0.4], fc='none', ec='k')
@@ -29,7 +29,7 @@ axins = fig.add_axes([0.625, 0.525, 0.30, 0.30], projection='ternary')
 # Limit the ploting range to be consistent with the above plotted triangle.
 axins.set_ternary_min(0.3, 0.3, 0.3)
 
-axins.scatter(t0, l0, r0)
-axins.scatter(t1, l1, r1)
+axins.scatter(t0, l0, r0, alpha=0.5)
+axins.scatter(t1, l1, r1, alpha=0.5)
 
 plt.show()
