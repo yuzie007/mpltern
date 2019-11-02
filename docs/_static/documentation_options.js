622d1478d200bb9a3fb1870483e529d6a5f2c542
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2019-11-02 20:16:12 +0100

    Update the website

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0+1.g3830ae7.dirty',
+    VERSION: '0.3.0+7.g13f81a3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
