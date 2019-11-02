13f81a33c437093a5774486dab4d2705bff0254a
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2019-11-02 20:08:49 +0100

    Modify the favicon to have the same sizes as that of Matplotlib
    Due to some reason, the previous favicon was not shown in Chrome, while
    it was correctly shown in Safari. Now the favicon sizes are modified to
    be the same as those of Matplotlib, which hopefully fixes this issue.

--- docsrc/create_favicon.py
+++ docsrc/create_favicon.py
@@ -2,4 +2,5 @@ from PIL import Image
 
 filename = r'../examples/miscellaneous/favicon.png'
 img = Image.open(filename)
-img.save('_static/favicon.ico')
+icon_sizes = [(16, 16), (32, 32), (48, 48)]
+img.save('_static/favicon.ico', sizes=icon_sizes)
