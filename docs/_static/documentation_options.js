35c4ce886a501c26c1b0589f24bdb163f68be716
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2019-11-06 11:08:30 +0100

    Update the website

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0+7.g13f81a3',
+    VERSION: '0.3.0+14.g0eaf453',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
