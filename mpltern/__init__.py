15d8690264d1d146ecd9cd522686eff42e574495
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2021-11-29 20:07:24 +0100

    Fix how to find the version number
    https://scrapbox.io/shimizukawa/setuptools_scm

--- mpltern/__init__.py
+++ mpltern/__init__.py
@@ -1,6 +1,19 @@
 from matplotlib.projections import register_projection
 from mpltern.ternary import TernaryAxes
 
-from ._version import version as __version__
+try:  # py38 or later
+    from importlib.metadata import version, PackageNotFoundError
+    try:
+        __version__ = version("mpltern")
+    except PackageNotFoundError:
+        # package is not installed
+        pass
+except ImportError:  # py36, py37
+    from pkg_resources import get_distribution, DistributionNotFound
+    try:
+        __version__ = get_distribution(__name__).version
+    except DistributionNotFound:
+        # package is not installed
+        pass
 
 register_projection(TernaryAxes)
