6577bfd0eddff71ed83745200f1867c1ab4dc0ea
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-02-19 22:45:17 +0900

    Reformat `test_given_triangles.py`

--- tests/test_given_triangles.py
+++ tests/test_given_triangles.py
@@ -6,30 +6,29 @@ from mpltern.ternary.datasets import get_spiral
 
 
 class TestGivenTriangles:
-    labelrotations = ['tick', 'axis', 'horizontal']
+    labelrotations = ["tick", "axis", "horizontal"]
     rotations = range(0, 360, 90)
-    expected = [(lr, r, ['given_triangles_{}_{}'.format(lr, r)])
-                for lr, r in product(labelrotations, rotations)]
+    expected = [
+        (lr, r, [f"given_triangles_{lr}_{r}"])
+        for lr, r in product(labelrotations, rotations)
+    ]
 
-    @pytest.mark.parametrize('labelrotation, rotation, baseline_images',
-                             expected)
-    @image_comparison(baseline_images=None, extensions=['pdf'], style='mpl20')
+    @pytest.mark.parametrize("labelrotation, rotation, baseline_images", expected)
+    @image_comparison(baseline_images=None, extensions=["pdf"], style="mpl20")
     def test_given_triangles(self, labelrotation, rotation, baseline_images):
         # Check if the tick-markers, tick-labels, and axis-labels are shown as
         # expected.
-        if 'text.kerning_factor' in plt.rcParams:
-            plt.rcParams['text.kerning_factor'] = 6
+        if "text.kerning_factor" in plt.rcParams:
+            plt.rcParams["text.kerning_factor"] = 6
 
-        fig = plt.figure()
         corners = ((0.5, 0.0), (1.0, 0.5), (0.0, 1.0))
-        ax = fig.add_subplot(
-            projection='ternary', corners=corners, rotation=rotation)
+        ax = plt.subplot(projection="ternary", corners=corners, rotation=rotation)
         t, l, r = get_spiral()
         ax.plot(t, l, r)
 
-        ax.set_tlabel('Top')
-        ax.set_llabel('Left')
-        ax.set_rlabel('Right')
+        ax.set_tlabel("Top")
+        ax.set_llabel("Left")
+        ax.set_rlabel("Right")
 
         ax.tick_params(labelrotation=labelrotation)
 
