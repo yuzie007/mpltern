d273150ffe2098b7cb084e9a45e2316f0c400a8d
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-02-19 22:25:14 +0900

    Add example for `hexbin`

---
+++ examples/intermediate/hexbin.py
@@ -0,0 +1,22 @@
+"""
+======
+HexBin
+======
+
+`~.Axes.hexbin` is a 2D histogram plot, in which the bins are hexagons and
+the color represents the number of data points within each bin.
+
+Unlike Matplotlib, `gridsize` can be only a single int.
+"""
+import numpy as np
+
+import matplotlib.pyplot as plt
+import mpltern
+
+np.random.seed(19680801)
+t, l, r = np.random.dirichlet(alpha=(2.0, 4.0, 8.0), size=100000).T
+ax = plt.subplot(projection="ternary")
+# If "face" (default), small hexagons look overlapping with each other.
+ax.hexbin(t, l, r, edgecolors="none")
+
+plt.show()
