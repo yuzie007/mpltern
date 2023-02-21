02b0cbf549b78a0ee71ee88cdbded58ea57e14a4
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-02-21 13:56:16 +0900

    Modify to use `np.testing`
    `np.testing.assert_array_equal` give a better message than
    `assert np.array_equal`.

--- tests/test_hexbin_helpers.py
+++ tests/test_hexbin_helpers.py
@@ -9,7 +9,7 @@ def test_calc_serial_index_of_hexagons():
     fname = os.path.join(os.path.dirname(__file__), fname)
     g, it, il, ir, i_ref = np.loadtxt(fname, dtype=int, unpack=True)
     i = hexbin_helpers.ternary_to_serial(g, it, il, ir)
-    assert np.array_equal(i, i_ref)
+    np.testing.assert_array_equal(i, i_ref)
 
 
 def test_calc_ternary_index_of_hexagons():
@@ -17,6 +17,6 @@ def test_calc_ternary_index_of_hexagons():
     fname = os.path.join(os.path.dirname(__file__), fname)
     g, it_ref, il_ref, ir_ref, i = np.loadtxt(fname, dtype=int, unpack=True)
     it, il, ir = hexbin_helpers.serial_to_ternary(g, i)
-    assert np.array_equal(it, it_ref)
-    assert np.array_equal(il, il_ref)
-    assert np.array_equal(ir, ir_ref)
+    np.testing.assert_array_equal(it, it_ref)
+    np.testing.assert_array_equal(il, il_ref)
+    np.testing.assert_array_equal(ir, ir_ref)
