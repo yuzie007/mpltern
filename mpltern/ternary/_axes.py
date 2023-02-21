e166ca39f99cac27af70c376ec846ec984f08b15
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-02-21 13:53:22 +0900

    Add `tribin` tuned for ternary plots

--- mpltern/ternary/_axes.py
+++ mpltern/ternary/_axes.py
@@ -11,6 +11,7 @@ from mpltern.ternary_parsers import (
     _parse_ternary_single, _parse_ternary_multiple,
     _parse_ternary_vector, _parse_ternary_vector_field)
 from mpltern import hexbin_helpers
+from mpltern import tribin_helpers
 from mpltern.ternary._base import TernaryAxesBase
 
 
@@ -506,6 +507,238 @@ class TernaryAxes(TernaryAxesBase):
             linewidths=linewidths,
             offsets=offsets,
             transOffset=mtransforms.AffineDeltaTransform(self.transData),
+        )
+
+        # Set normalizer if bins is 'log'
+        if bins == 'log':
+            if norm is not None:
+                _api.warn_external("Only one of 'bins' and 'norm' arguments "
+                                   f"can be supplied, ignoring bins={bins}")
+            else:
+                norm = mcolors.LogNorm(vmin=vmin, vmax=vmax)
+                vmin = vmax = None
+            bins = None
+
+        # autoscale the norm with current accum values if it hasn't been set
+        if norm is not None:
+            if norm.vmin is None and norm.vmax is None:
+                norm.autoscale(accum)
+
+        if bins is not None:
+            if not np.iterable(bins):
+                minimum, maximum = min(accum), max(accum)
+                bins -= 1  # one less edge than bins
+                bins = minimum + (maximum - minimum) * np.arange(bins) / bins
+            bins = np.sort(bins)
+            accum = bins.searchsorted(accum)
+
+        collection.set_array(accum)
+        collection.set_cmap(cmap)
+        collection.set_norm(norm)
+        collection.set_alpha(alpha)
+        collection.update(kwargs)  # matplotlib/matplotlib#22451
+        collection._scale_norm(norm, vmin, vmax)
+
+        # add the collection last
+        self.add_collection(collection, autolim=False)
+        return collection
+
+    def tribin(self, t, l, r, C=None, gridsize=100, bins=None,
+               xscale='linear', yscale='linear', extent=None,
+               cmap=None, norm=None, vmin=None, vmax=None,
+               alpha=None, linewidths=None, edgecolors='face',
+               reduce_C_function=np.mean, mincnt=None, marginals=False,
+               **kwargs):
+        """
+        Make a 2D triangular binning plot of points *t*, *l*, *r*.
+
+        If *C* is *None*, the value of the triangle is determined by the number
+        of points in the triangle. Otherwise, *C* specifies values at the
+        coordinate (x[i], y[i]). For each triangle, these values are reduced
+        using *reduce_C_function*.
+
+        Parameters
+        ----------
+        t, l, r : array-like
+            The data positions. *t*, *l*, and *r* must be of the same length.
+
+        C : array-like, optional
+            If given, these values are accumulated in the bins. Otherwise,
+            every point has a value of 1. Must be of the same length as *t*,
+            *l*, and *r*.
+
+        gridsize : int, default: 100
+            Number of triangles in one direction between min and max.
+            In mpltern, all the three directions have the same gridsize.
+
+        bins : 'log' or int or sequence, default: None
+            Discretization of the hexagon values.
+
+            - If *None*, no binning is applied; the color of each triangle
+              directly corresponds to its count value.
+            - If 'log', use a logarithmic scale for the colormap.
+              Internally, :math:`log_{10}(i+1)` is used to determine the
+              triangle color. This is equivalent to ``norm=LogNorm()``.
+            - If an integer, divide the counts in the specified number
+              of bins, and color the triangles accordingly.
+            - If a sequence of values, the values of the lower bound of
+              the bins to be used.
+
+        xscale : {'linear', 'log'}, default: 'linear'
+            Ignored in mpltern and always linear scales on all three axes.
+
+        yscale : {'linear', 'log'}, default: 'linear'
+            Ignored in mpltern and always linear scales on all three axes.
+
+        mincnt : int > 0, default: *None*
+            If not *None*, only display cells with more than *mincnt*
+            number of points in the cell.
+
+        marginals : bool, default: *False*
+            Ignored in mpltern.
+
+        extent : 6-tuple of float, default: *None*
+            The limits of the bins (tmin, tmax, lmin, lmax, rmin, rmax).
+            The default assigns the limits based on *tlim*, *llim*, *rlim*.
+
+        Returns
+        -------
+        `~matplotlib.collections.PolyCollection`
+            A `.PolyCollection` defining the triangular bins.
+
+            - `.PolyCollection.get_offsets` contains a Mx2 array containing
+              the x, y positions of the M triangle centers.
+            - `.PolyCollection.get_array` contains the values of the M
+              triangles.
+
+        Other Parameters
+        ----------------
+        %(cmap_doc)s
+
+        %(norm_doc)s
+
+        %(vmin_vmax_doc)s
+
+        alpha : float between 0 and 1, optional
+            The alpha blending value, between 0 (transparent) and 1 (opaque).
+
+        linewidths : float, default: *None*
+            If *None*, defaults to 1.0.
+
+        edgecolors : {'face', 'none', *None*} or color, default: 'face'
+            The color of the triangle edges. Possible values are:
+
+            - 'face': Draw the edges in the same color as the fill color.
+            - 'none': No edges are drawn. This can sometimes lead to unsightly
+              unpainted pixels between the triangles.
+            - *None*: Draw outlines in the default color.
+            - An explicit color.
+
+        reduce_C_function : callable, default: `numpy.mean`
+            The function to aggregate *C* within the bins. It is ignored if
+            *C* is not given. This must have the signature::
+
+                def reduce_C_function(C: array) -> float
+
+            Commonly used functions are:
+
+            - `numpy.mean`: average of the points
+            - `numpy.sum`: integral of the point values
+            - `numpy.amax`: value taken from the largest point
+
+        data : indexable object, optional
+            DATA_PARAMETER_PLACEHOLDER
+
+        **kwargs : `~matplotlib.collections.PolyCollection` properties
+            All other keyword arguments are passed on to `.PolyCollection`:
+
+            %(PolyCollection:kwdoc)s
+        """
+        self._process_unit_info([("t", t), ("l", l), ("r", r)], kwargs, convert=False)
+
+        t, l, r, C = cbook.delete_masked_points(t, l, r, C)
+
+        # Count the number of data in each triangle
+        t = np.asarray(t, float)
+        l = np.asarray(l, float)
+        r = np.asarray(r, float)
+
+        if extent is not None:
+            tmin, tmax, lmin, lmax, rmin, rmax = extent
+        else:
+            tmin, tmax = self.get_tlim()
+            lmin, lmax = self.get_llim()
+            rmin, rmax = self.get_rlim()
+
+        # number of triangles
+        n = gridsize ** 2
+
+        st, sl, sr, it, il, ir = tribin_helpers.calc_ternary_indices(
+            t, l, r, gridsize, (tmin, tmax, lmin, lmax, rmin, rmax))
+
+        # flat indices, plus one so that out-of-range points go to position 0.
+        indices = tribin_helpers.ternary_to_serial(gridsize, it, il, ir) + 1
+
+        if C is None:  # [1:] drops out-of-range points.
+            counts = np.bincount(indices, minlength=1 + n)[1:]
+            accum = counts.astype(float)
+            if mincnt is not None:
+                accum[accum < mincnt] = np.nan
+        else:
+            # store the C values in a list per hexagon index
+            Cs = [[] for _ in range(1 + n)]
+            for i in range(len(C)):
+                Cs[indices[i]].append(C[i])
+            if mincnt is None:
+                mincnt = 0
+            accum = np.array(
+                [reduce_C_function(acc) if len(acc) > mincnt else np.nan
+                 for acc in Cs[1:]],  # [1:] drops out-of-range points.
+                float)
+
+        good_idxs = ~np.isnan(accum)
+
+        _ = tribin_helpers.serial_to_ternary(gridsize, np.arange(n))
+        offsets = np.array(_).T.astype(float)
+
+        # remove accumulation bins with no data
+        offsets = offsets[good_idxs, :]
+        accum = accum[good_idxs]
+
+        polygons = np.repeat(offsets[:, None, :], 3, axis=1)
+
+        # number of upward triangles
+        nup = gridsize * (gridsize + 1) // 2
+
+        # upward triangles
+        polygons[:nup, 0, :] += [1, 0, 0]
+        polygons[:nup, 1, :] += [0, 1, 0]
+        polygons[:nup, 2, :] += [0, 0, 1]
+
+        # downward triangles
+        polygons[nup:, 0, :] += [0, 1, 1]
+        polygons[nup:, 1, :] += [1, 0, 1]
+        polygons[nup:, 2, :] += [1, 1, 0]
+
+        # scale into [0, 1]
+        polygons /= gridsize
+
+        # scale into the given extent
+        polygons[..., 0] = (1.0 - polygons[..., 0]) * tmin + polygons[..., 0] * tmax
+        polygons[..., 1] = (1.0 - polygons[..., 1]) * lmin + polygons[..., 1] * lmax
+        polygons[..., 2] = (1.0 - polygons[..., 2]) * rmin + polygons[..., 2] * rmax
+
+        if linewidths is None:
+            linewidths = [1.0]
+
+        polygons = polygons.reshape(-1, 3)
+        polygons = self.transProjection.transform(polygons)
+        polygons = polygons.reshape(-1, 3, 2)
+
+        collection = mcoll.PolyCollection(
+            polygons,
+            edgecolors=edgecolors,
+            linewidths=linewidths,
         )
 
         # Set normalizer if bins is 'log'
---
+++ tests/test_tribin.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.testing.decorators import image_comparison
+import mpltern
+
+@image_comparison(
+    baseline_images=["base"],
+    extensions=["pdf"],
+    style="mpl20",
+)
+def test_base():
+    np.random.seed(19680801)
+    t, l, r = np.random.dirichlet(alpha=(2.0, 4.0, 8.0), size=100000).T
+    ax = plt.subplot(projection="ternary")
+    # If "face" (default), small hexagons look overlapping with each other.
+    ax.tribin(t, l, r, edgecolors="none")
+
+@image_comparison(
+    baseline_images=["ternary_lim"],
+    extensions=["pdf"],
+    style="mpl20",
+)
+def test_ternary_lim():
+    np.random.seed(19680801)
+    t, l, r = np.random.dirichlet(alpha=(2.0, 4.0, 8.0), size=100000).T
+    ax = plt.subplot(projection="ternary")
+    # If "face" (default), small hexagons look overlapping with each other.
+    ax.tribin(t, l, r, edgecolors="none")
+    ax.set_ternary_lim(
+        0.1, 0.5,  # tmin, tmax
+        0.2, 0.6,  # lmin, lmax
+        0.3, 0.7,  # rmin, rmax
+    )
+
+@image_comparison(
+    baseline_images=["extent"],
+    extensions=["pdf"],
+    style="mpl20",
+)
+def test_extent():
+    np.random.seed(19680801)
+    t, l, r = np.random.dirichlet(alpha=(2.0, 4.0, 8.0), size=100000).T
+    ax = plt.subplot(projection="ternary")
+    extent = (0.1, 0.5, 0.2, 0.6, 0.3, 0.7)
+    # If "face" (default), small hexagons look overlapping with each other.
+    ax.tribin(t, l, r, gridsize=40, extent=extent, edgecolors="none")
+
+@image_comparison(
+    baseline_images=["given_triangles"],
+    extensions=["pdf"],
+    style="mpl20",
+)
+def test_given_triangles():
+    corners = ((0.5, 0.0), (1.0, 0.5), (0.0, 1.0))
+    np.random.seed(19680801)
+    t, l, r = np.random.dirichlet(alpha=(2.0, 4.0, 8.0), size=100000).T
+    ax = plt.subplot(projection="ternary", corners=corners, rotation=0)
+    # If "face" (default), small hexagons look overlapping with each other.
+    ax.tribin(t, l, r, edgecolors="none")
