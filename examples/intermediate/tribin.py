e3f7d86a26ade76df8a0879de08ecf05575e748f
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-02-21 14:28:21 +0900

    Add example for `tribin`

--- examples/intermediate/tribin.py
+++ examples/intermediate/tribin.py
@@ -1,9 +1,9 @@
 """
 ======
-HexBin
+TriBin
 ======
 
-``ax.hexbin`` is a 2D histogram plot, in which the bins are hexagons and
+``ax.tribin`` is a 2D histogram plot, in which the bins are triangles and
 the color represents the number of data points within each bin.
 
 Unlike Matplotlib, `gridsize` can be only a single int.
@@ -16,7 +16,7 @@ import mpltern
 np.random.seed(19680801)
 t, l, r = np.random.dirichlet(alpha=(2.0, 4.0, 8.0), size=100000).T
 ax = plt.subplot(projection="ternary")
-# If "face" (default), small hexagons look overlapping with each other.
-ax.hexbin(t, l, r, edgecolors="none")
+# If "face" (default), small triangles look overlapping with each other.
+ax.tribin(t, l, r, edgecolors="none")
 
 plt.show()
