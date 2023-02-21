0c2596da609fa8684434828a6834a44b085dc8ce
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-02-22 01:38:39 +0900

    Modify to use the Dirichlet distribution

--- examples/intermediate/legend.py
+++ examples/intermediate/legend.py
@@ -5,14 +5,18 @@ Legend
 
 The legend can be put using ``ax.legend`` in the same way as Matplotlib.
 """
+import numpy as np
+
 import matplotlib.pyplot as plt
-from mpltern.datasets import get_scatter_points
+import mpltern
 
 
-ax = plt.subplot(projection='ternary')
+ax = plt.subplot(projection="ternary")
 
-for seed in [1, 9, 6, 8]:
-    ax.scatter(*get_scatter_points(11, seed=seed), alpha=0.5, label=seed)
+np.random.seed(19680801)
+for i in range(4):
+    t, l, r = np.random.dirichlet(alpha=(2.0, 2.0, 2.0), size=10).T
+    ax.scatter(t, l, r, alpha=0.5, label=i)
 
 ax.legend()
 
