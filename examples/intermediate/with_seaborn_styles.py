6e5f16646c00cb488dede8b5e664e896d3fd3a29
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-04-30 09:26:13 +0200

    Modify not to use seaborn for examples

--- examples/intermediate/with_seaborn_styles.py
+++ examples/intermediate/with_seaborn_styles.py
@@ -1,23 +1,41 @@
 """
-===================
-With seaborn styles
-===================
+============
+Style sheets
+============
 
-You can use seaborn styles with mpltern in the same way as Matplotlib.
+`You can use style sheets in the same way as Matplotlib.
+<https://matplotlib.org/stable/tutorials/introductory/customizing.html
+#using-style-sheets>`__
+This applies also for the style sheets of third-party packages like
+`seaborn <https://seaborn.pydata.org/generated/seaborn.set_style.html>`__.
 """
 import matplotlib.pyplot as plt
 from mpltern.datasets import get_spiral
-import seaborn as sns
 
+# %%
 
-sns.set_style('darkgrid')
+plt.style.use("ggplot")
 
-ax = plt.subplot(projection='ternary')
+ax = plt.subplot(projection="ternary")
 
 ax.plot(*get_spiral())
 
-ax.set_tlabel('Top')
-ax.set_llabel('Left')
-ax.set_rlabel('Right')
+ax.set_tlabel("Top")
+ax.set_llabel("Left")
+ax.set_rlabel("Right")
+
+plt.show()
+
+# %%
+
+plt.style.use("dark_background")
+
+ax = plt.subplot(projection="ternary")
+
+ax.plot(*get_spiral())
+
+ax.set_tlabel("Top")
+ax.set_llabel("Left")
+ax.set_rlabel("Right")
 
 plt.show()
