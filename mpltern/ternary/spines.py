f98bac897ad5d74985b1c5e33359babd581640c5
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-05-04 22:00:48 +0200

    Modify to use ternary transformations

--- mpltern/ternary/spines.py
+++ mpltern/ternary/spines.py
@@ -1,49 +1,40 @@
+"""Spine"""
 import matplotlib as mpl
 import matplotlib.path as mpath
 import matplotlib.spines as mspines
 
 
 class Spine(mspines.Spine):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.set_transform(self.axes.transAxes)
+    def __init__(self, axes, spine_type, path, **kwargs):
+        super().__init__(axes, spine_type, path, **kwargs)
+        if spine_type == "tside":
+            self.set_transform(self.axes.get_taxis_transform())
+        elif spine_type == "lside":
+            self.set_transform(self.axes.get_laxis_transform())
+        elif spine_type == "rside":
+            self.set_transform(self.axes.get_raxis_transform())
+        else:
+            raise ValueError(f'unknown spine_type: {spine_type}')
 
     def _adjust_location(self):
         """Automatically set spine bounds to the view interval."""
-        corners = [[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]]
-        corners_axes = self.axes.transAxesProjection.transform(corners)
-
-        v1 = self._path.vertices
-        assert v1.shape == (2, 2), 'unexpected vertices shape'
-        if self.spine_type in ['tside']:
-            v1[0] = corners_axes[1]
-            v1[1] = corners_axes[2]
-        elif self.spine_type in ['lside']:
-            v1[0] = corners_axes[2]
-            v1[1] = corners_axes[0]
-        elif self.spine_type in ['rside']:
-            v1[0] = corners_axes[0]
-            v1[1] = corners_axes[1]
+        if self.spine_type == "tside":
+            low = self.axes.get_tlim()[0]
+        elif self.spine_type == "lside":
+            low = self.axes.get_llim()[0]
+        elif self.spine_type == "rside":
+            low = self.axes.get_rlim()[0]
         else:
-            raise ValueError(f'unknown spine spine_type: {self.spine_type}')
+            raise ValueError
+        self._path.vertices = [[low, 0.0], [low, 1.0]]
 
     def get_spine_transform(self):
-        return self.axes.transAxes
+        return self.get_transform()
 
     @classmethod
     def linear_spine(cls, axes, spine_type, **kwargs):
         """Create and return a linear `Spine`."""
-        # all values of 0.999 get replaced upon call to set_bounds()
-        if spine_type == 'tside':
-            path = mpath.Path([(0.0, 0.0), (1.0, 0.0)])
-        elif spine_type == 'lside':
-            path = mpath.Path([(0.5, 1.0), (0.0, 0.0)])
-        elif spine_type == 'rside':
-            path = mpath.Path([(1.0, 0.0), (0.5, 1.0)])
-        else:
-            raise ValueError('unable to make path for spine "%s"' % spine_type)
+        path = mpath.Path([(0.0, 0.0), (0.0, 1.0)])
         result = cls(axes, spine_type, path, **kwargs)
         result.set_visible(mpl.rcParams['axes.spines.bottom'])
-
         return result
-
