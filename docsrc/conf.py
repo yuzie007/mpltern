e65ce4c1d6c8aa990ce196f3254b69e190ad5ab7
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-06-04 01:52:55 +0200

    Add link to GitHub

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -170,6 +170,10 @@ github_project_url = "https://github.com/yuzie007/mpltern/"
 
 html_theme = "pydata_sphinx_theme"
 
+html_theme_options = {
+    "github_url": "https://github.com/yuzie007/mpltern",
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
