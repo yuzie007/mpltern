c52129de0c4a8e4df53742f8f4e8fa8f856e66f7
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-06-06 23:08:47 +0200

    Add `tick-formatters.py`

---
+++ examples/axis_and_tick/tick-formatters.py
@@ -0,0 +1,36 @@
+"""
+===============
+Tick formatters
+===============
+
+.. |PercentFormatter| replace:: ``PercentFormatter``
+.. _PercentFormatter: https://matplotlib.org/stable/api/ticker_api.html#matplotlib.ticker.PercentFormatter
+
+`Tick formatters can be given in the same way as Matplotlib
+<https://matplotlib.org/stable/gallery/ticks/tick-formatters.html>`__.
+Among available formatters, |PercentFormatter|_ may be particularly useful for
+ternary plots.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
+import mpltern
+
+constant = 100.0
+
+ax = plt.subplot(projection="ternary", constant=constant)
+
+np.random.seed(19680801)
+t, l, r = constant * np.random.dirichlet(alpha=(2.0, 4.0, 8.0), size=128).T
+
+ax.scatter(t, l, r, s=64.0, c="none", edgecolors="C0")
+
+ax.set_tlabel("$x_1$")
+ax.set_llabel("$x_2$")
+ax.set_rlabel("$x_3$")
+
+ax.taxis.set_major_formatter(PercentFormatter())
+ax.laxis.set_major_formatter(PercentFormatter())
+ax.raxis.set_major_formatter(PercentFormatter())
+
+plt.show()
