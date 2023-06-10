a23fadc5732f5dd40f97459f9e1740c7a1c833d4
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-06-10 20:14:12 +0200

    Add more examples for ternary limits

---
+++ examples/limits/fit.py
@@ -0,0 +1,68 @@
+"""
+=======================
+Fit the plotting region
+=======================
+
+When the ternary limits are modified, the plotting region can be expanded or
+cropped in a different way via the `fit` option in e.g. `set_ternary_lim`.
+
+- `'rectangle'` (default): Fitted to the original rectangle.
+- `'triangle'`: Fitted to the original triangle.
+- `'none'`: The plotting region is simply cropped (or expanded).
+"""
+import matplotlib.pyplot as plt
+from mpltern.datasets import get_spiral
+
+
+def set_labels(ax):
+    """Set ternary-axis labels."""
+    ax.set_tlabel('Top')
+    ax.set_llabel('Left')
+    ax.set_rlabel('Right')
+    ax.taxis.set_label_position("tick1")
+    ax.laxis.set_label_position("tick1")
+    ax.raxis.set_label_position("tick1")
+
+
+def plot_frame(ax):
+    ax.fill(
+        [0.0, 0.0, 1.0, 1.0],
+        [0.0, 1.0, 1.0, 0.0],
+        ec="r",
+        fc="none",
+        ls=":",
+        transform=ax.transAxes,
+        clip_on=False,
+        zorder=1.0,
+    )
+    ax.fill(
+        [0.5, 0.0, 1.0],
+        [1.0, 0.0, 0.0],
+        ec="b",
+        fc="none",
+        ls=":",
+        transform=ax.transAxes,
+        clip_on=False,
+        zorder=1.0,
+    )
+
+
+fig = plt.figure(figsize=(10.8, 4.8))
+fig.subplots_adjust(left=0.075, right=0.925, wspace=0.25)
+
+fits = ["rectangle", "triangle", "none"]
+for i, fit in enumerate(fits):
+    ax = fig.add_subplot(1, 3, i + 1, projection='ternary')
+    ax.plot(*get_spiral(), c="k")
+    ax.set_ternary_lim(
+        0.1, 0.7,  # tmin, tmax
+        0.1, 0.7,  # lmin, lmax
+        0.1, 0.7,  # rmin, rmax
+        fit=fit,
+    )
+    set_labels(ax)
+    ax.set_title(f"fit='{fit}'", pad=42)
+
+    plot_frame(ax)
+
+plt.show()
---
+++ examples/limits/hexagonal_limits.py
@@ -0,0 +1,81 @@
+"""
+================
+Hexagonal limits
+================
+
+Suppose we would like to crop or expand the following gray-shaded region.
+
+.. plot::
+    :context: reset
+    :include-source: false
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from mpltern.datasets import get_spiral
+
+    ax = plt.subplot(projection='ternary')
+    ax.plot(*get_spiral(), color="k")
+    points = np.array([
+        [0.7, 0.1, 0.2],
+        [0.7, 0.2, 0.1],
+        [0.2, 0.7, 0.1],
+        [0.1, 0.7, 0.2],
+        [0.1, 0.2, 0.7],
+        [0.2, 0.1, 0.7],
+    ])
+    ax.fill(points[:, 0], points[:, 1], points[:, 2], color="0.6")
+
+    ax.set_tlabel("T")
+    ax.set_llabel("L")
+    ax.set_rlabel("R")
+
+The region is defined as the intersect of :math:`t \\in [0.1, 0.7]`,
+:math:`l \\in [0.1, 0.7]`, :math:`r \\in [0.1, 0.7]`.
+
+.. plot::
+    :context:
+    :include-source: false
+
+    ax.axtspan(0.1, 0.7, color="C0", fc="none", hatch="....")
+    ax.axlspan(0.1, 0.7, color="C1", fc="none", hatch="....")
+    ax.axrspan(0.1, 0.7, color="C2", fc="none", hatch="....")
+"""
+# %%
+# Using `set_ternary_lim`, we explicitly specify the min and the max values
+# for all the ternary axes.
+import matplotlib.pyplot as plt
+from mpltern.datasets import get_spiral
+
+ax = plt.subplot(projection='ternary')
+
+ax.plot(*get_spiral(), color="k")
+
+ax.set_ternary_lim(
+    0.1, 0.7,  # tmin, tmax
+    0.1, 0.7,  # lmin, lmax
+    0.1, 0.7,  # rmin, rmax
+)
+
+ax.set_tlabel("T")
+ax.set_llabel("L")
+ax.set_rlabel("R")
+
+# For a hexagonal plotting region, it may be better to put the axis labels on
+# the sides (either "tick1" or "tick2") rather than the default "corner" to
+# avoid confusion.
+ax.taxis.set_label_position("tick1")
+ax.laxis.set_label_position("tick1")
+ax.raxis.set_label_position("tick1")
+
+plt.show()
+# %%
+# We can also set the min and the max values of each ternary axis one by one.
+ax = plt.subplot(projection='ternary')
+
+ax.plot(*get_spiral(), color="k")
+
+ax.set_tlim(0.1, 0.7)
+ax.set_llim(0.1, 0.7)
+ax.set_rlim(0.1, 0.7)
+
+plt.show()
