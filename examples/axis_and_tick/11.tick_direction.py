e5bea2f98a8cfade568af6434cbf3817b1cbe6b3
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-06-11 09:36:13 +0200

    Modify figure spacing etc.

--- examples/axis_and_tick/11.tick_direction.py
+++ examples/axis_and_tick/11.tick_direction.py
@@ -3,25 +3,18 @@
 Tick direction
 ==============
 
-Tick direction can be changed via
+Tick direction can be modified via
 `ax.tick_params(direction={'in', 'out', 'inout'})`.
 """
 import matplotlib.pyplot as plt
 import mpltern
 
-fig = plt.figure(figsize=(14.4, 4.8))
-fig.subplots_adjust(left=0.075, right=0.925, wspace=0.3)
+fig = plt.figure(figsize=(10.8, 4.8))
+fig.subplots_adjust(left=0.075, right=0.925, wspace=0.25)
 
-ax = fig.add_subplot(131, projection='ternary')
-ax.tick_params(direction='in')
-ax.set_title("ax.tick_params(direction='in')", pad=36)
-
-ax = fig.add_subplot(132, projection='ternary')
-ax.tick_params(direction='out')
-ax.set_title("ax.tick_params(direction='out')", pad=36)
-
-ax = fig.add_subplot(133, projection='ternary')
-ax.tick_params(direction='inout')
-ax.set_title("ax.tick_params(direction='inout')", pad=36)
+for i, direction in enumerate(["in", "out", "inout"]):
+    ax = fig.add_subplot(1, 3, i + 1, projection='ternary')
+    ax.tick_params(direction=direction)
+    ax.set_title(f"ax.tick_params(direction='{direction}')", pad=36)
 
 plt.show()
--- examples/axis_and_tick/12.tick_rotation.py
+++ examples/axis_and_tick/12.tick_rotation.py
@@ -3,25 +3,18 @@
 Tick rotation
 =============
 
-Tick rotation can be changed via
+Tick rotation can be modified via
 `ax.tick_params(labelrotation={'tick', 'axis', 'horizontal'})`.
 """
 import matplotlib.pyplot as plt
 import mpltern
 
-fig = plt.figure(figsize=(14.4, 4.8))
-fig.subplots_adjust(left=0.075, right=0.925, wspace=0.3)
+fig = plt.figure(figsize=(10.8, 4.8))
+fig.subplots_adjust(left=0.075, right=0.925, wspace=0.25)
 
-ax = fig.add_subplot(131, projection='ternary')
-ax.tick_params(labelrotation='tick')
-ax.set_title("labelrotation='tick'", pad=36)
-
-ax = fig.add_subplot(132, projection='ternary')
-ax.tick_params(labelrotation='axis')
-ax.set_title("labelrotation='axis'", pad=36)
-
-ax = fig.add_subplot(133, projection='ternary')
-ax.tick_params(labelrotation='horizontal')
-ax.set_title("labelrotation='horizontal'", pad=36)
+for i, labelrotation in enumerate(["tick", "axis", "horizontal"]):
+    ax = fig.add_subplot(1, 3, i + 1, projection='ternary')
+    ax.tick_params(labelrotation=labelrotation)
+    ax.set_title(f"labelrotation='{labelrotation}'", pad=36)
 
 plt.show()
