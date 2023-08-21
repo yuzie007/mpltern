7f59eba5fdbf1397ca6e4c52c234eccd347fd9c1
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-08-22 00:13:55 +0200

    Fix `C` to include the values exactly `mincnt`
    - matplotlib/matplotlib#26113

--- mpltern/ternary/_axes.py
+++ mpltern/ternary/_axes.py
@@ -466,7 +466,7 @@ class TernaryAxes(TernaryAxesBase):
             if mincnt is None:
                 mincnt = 0
             accum = np.array(
-                [reduce_C_function(acc) if len(acc) > mincnt else np.nan
+                [reduce_C_function(acc) if len(acc) >= mincnt else np.nan
                  for acc in Cs[1:]],  # [1:] drops out-of-range points.
                 float)
 
@@ -706,7 +706,7 @@ class TernaryAxes(TernaryAxesBase):
             if mincnt is None:
                 mincnt = 0
             accum = np.array(
-                [reduce_C_function(acc) if len(acc) > mincnt else np.nan
+                [reduce_C_function(acc) if len(acc) >= mincnt else np.nan
                  for acc in Cs[1:]],  # [1:] drops out-of-range points.
                 float)
 
