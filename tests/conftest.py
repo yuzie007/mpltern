d0fc438296a25ce97a9bc587ac2b4627ceb94e8a
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-04-27 00:28:30 +0200

    Remove `mpltern_test_settings`

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,1 @@
-from matplotlib.testing.conftest import *
-
-
-@pytest.fixture
-def mpltern_test_settings():
-    # `text.kerning_factor` introduced since Matplotlib 3.2.0, changes default
-    # text positions. To be compatible with baseline_images, the old behavior
-    # is restored.
-    if 'text.kerning_factor' in matplotlib.rcParams:
-        matplotlib.rcParams['text.kerning_factor'] = 6
+from matplotlib.testing.conftest import mpl_test_settings
