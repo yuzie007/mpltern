0c3c628e4d00359072846166df3d004bd28513da
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2026-06-26 20:17:01 +0200

    Set larger `tol` for `test_XXXlayout.py`

--- tests/test_constrainedlayout.py
+++ tests/test_constrainedlayout.py
@@ -1,10 +1,14 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.testing.decorators import image_comparison
 
 import mpltern
 
+mpl_version = tuple(int(_) for _ in mpl.__version__.split('.'))
+tol = 20.0 if mpl_version >= (3, 11) else 0.0
 
-@image_comparison(["constrained_layout1"], extensions=["pdf"], style="mpl20")
+
+@image_comparison(["constrained_layout1"], extensions=["pdf"], tol=tol, style="mpl20")
 def test_constrained_layout1():
     plt.rcParams['text.kerning_factor'] = 6
 
